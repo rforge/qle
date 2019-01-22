405d8c4a31d6872aff4b6e4865c2acfedddbb6d1
bahama <bahama@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-01-22 13:02:47 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qle@344 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/qle-cran/tests/test_quasiDeviance.R
+++ pkg/qle-cran/tests/test_quasiDeviance.R
@@ -11,24 +11,30 @@
 library(qle)
 data(normal)
 
+# type of variance matrix interpolation
+qsd$var.type
+
 # design, statistics, predictions
 x0 <- c("mu"=2,"sigma"=1)
 Xs <- as.matrix(qsd$qldata[c(1,2)])
 Tstat <- qsd$qldata[c(3,4)]
 pred <- estim(qsd$covT,x0,Xs,Tstat,krig.type="var")[[1]]
-pred2 <- estim(qsd$covT,x0,Xs,Tstat,krig.type="dual")[[1]]
+
+## dual kriging
+# pred2 <- estim(qsd$covT,x0,Xs,Tstat,krig.type="dual")[[1]]
 
 # compute quasi-deviance with use of kriging variances
 D <- quasiDeviance(x0,qsd,verbose=TRUE)[[1]]
 
-# quasi-score
-S <- attr(D,"Sigma")
+# remove kriging variances from variance matrix approximation
+S <- attr(D,"Sigma") - diag(pred$sigma2)
 invS <- solve(S)
 B <- invS%*%t(D$jac)
 
-# prediction variances
-# of sample mean of statistic Z=E[T(X)]
+# kriging prediction variances of sample means of statistics
 stopifnot(D$sig2==pred$sigma2)
+
+# quasi-score vector
 D$score
 (qs <- (qsd$obs-pred$mean)%*%B)
 
@@ -36,18 +42,19 @@ D$score
 D$I
 t(B)%*%(S+diag(D$sig2))%*%B
 
-# variance quasi-score vector
+# variance of quasi-score vector (only within the kriging model)
 D$varS
 (C <- t(B)%*%diag(D$sig2)%*%B)
 
 # variance matrix of statistics Var(T(X))
 print(S)
-covarTx(qsd,theta=x0)
+covarTx(qsd)			# no added kriging variances
+covarTx(qsd,theta=x0)   # adding kriging variances at theta
 
-# value quasi-deviance
+# modified quasi-deviance value based on modified quasi-information matrix
 D$value
 qs%*%solve(D$I)%*%t(qs)
 
-# modified quasi-information: Mahalanobis distance of quasi-score
+# modified quasi-deviance value based on kriging errot of quasi-score
 D$qval
 qs%*%solve(C)%*%t(qs)
