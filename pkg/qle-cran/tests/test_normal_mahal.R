2d365af3fb871bff95ab3544810d016776452e0c
bahama <bahama@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-01-22 14:52:27 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qle@351 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/qle-cran/tests/test_normal_mahal.R
+++ pkg/qle-cran/tests/test_normal_mahal.R
@@ -34,12 +34,10 @@ attr(sim,"nsim") <- 100
 qsd <- getQLmodel(sim,lb,ub,obs,var.type="wcholMean",
 		model="sirfk", criterion="mahal", verbose=TRUE)
 
-# force only global searches and testing
-#debug(qle)
+# only accept numerically zero quasi-scores (gradient of MD)
 OPT <- qle(qsd,simfunc,nsim=10,
 		global.opts=list("stopval"=0,"maxeval"=3),
-		local.opts=list("ftol_abs"=0),
-		method=c("bobyqa","cobyla","direct"),pl=2)
+		local.opts=list("ftol_abs"=0), method="bobyqa",pl=2)
 
 OPT$final
 OPT$why
@@ -47,7 +45,7 @@ OPT$why
 ## testing with criterion `mahal`
 ## here: no Iobs for best root selection
 S0 <- multiSearch(theta0, qsd=OPT$qsd, method=c("bobyqa","cobyla","direct"),
-		 nstart=5,	multi.start=TRUE, optInfo=TRUE, pl=10, verbose=TRUE)
+		 nstart=15,	multi.start=TRUE, optInfo=TRUE, pl=10, verbose=TRUE)
  
 ## found roots
 attr(S0,"roots")
